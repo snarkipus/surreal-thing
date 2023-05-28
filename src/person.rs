cfaed169f05b1739163c4386f0058283bb409b8d
Matt Jackson <matt@jacksonsix.com>
2023-05-27 20:05:58 -0400

    getting closer - why is this so hard?

--- src/person.rs
+++ src/person.rs
@@ -51,7 +51,11 @@ pub async fn list(db: Db) -> Result<Json<Vec<Person>>, Error> {
 
 #[cfg(test)]
 mod tests {
-    use surrealdb::{engine::remote::ws::Ws, opt::auth::Root, sql::Thing};
+    use surrealdb::{
+        engine::remote::ws::Ws,
+        opt::auth::Root,
+        sql::{Id, Thing},
+    };
 
     use super::*;
 
@@ -78,26 +82,115 @@ mod tests {
     #[tokio::test]
     async fn create_person() {
         let db = create_db().await;
-    
+
         // Simple stuff - SurrealDB handles creating the uuid() in the database
         let sql = "CREATE person:uuid() CONTENT { name: $name }";
-    
-        let mut res = db
-            .query(sql)
-            .bind(("name", "Blaze"))            
-            .await
-            .unwrap();
-        
-        // This just sucks - so much unwrapping 
-        let person: PersonModel = res.take(0)
+
+        let mut res = db.query(sql).bind(("name", "Blaze")).await.unwrap();
+
+        // This just sucks - so much unwrapping
+        let person: PersonModel = res
+            .take(0)
             .map(|p: Option<PersonModel>| p.unwrap())
             .unwrap();
-        
+
         // This is actually a Thing
         let id = &person.id.unwrap();
         let name = &person.name;
         assert_eq!(id.tb, "person");
         assert_eq!(name, "Blaze");
         println!("id: {}", &id.id.to_raw());
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct LicenseModel {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        id: Option<Thing>,
+        license_number: usize,
+    }
+
+    #[tokio::test]
+    async fn create_license() {
+        let db = create_db().await;
+
+        // create license number
+        let license_number: usize = 12345;
+
+        // create new person: Doc McStuffins
+        let sql = "CREATE person:uuid() CONTENT { name: $name }";
+        let mut res = db.query(sql).bind(("name", "McStuffins")).await.unwrap();
+
+        let _person_id = res
+            .take(0)
+            .map(|p: Option<PersonModel>| p.unwrap())
+            .map(|p: PersonModel| p.id.unwrap())
+            .map(|t: Thing| t.id)
+            .map(|id: Id| id.to_raw())
+            .unwrap();
+
+        // create new license
+        let sql = "CREATE registry:uuid() CONTENT { registration: $license_number }";
+
+        let _res = db
+            .query(sql)
+            .bind(("license_number", license_number))
+            .await
+            .unwrap();
+
+        // relate license to person
+        let sql = "
+            LET $foo = SELECT id FROM person WHERE name = $name;
+            LET $bar = SELECT id FROM registry WHERE registration = $license_number;
+            RELATE $bar->licenses->$foo SET id = licenses:uuid();
+        ";
+
+        let _res = db
+            .query(sql)
+            .bind(("name", "McStuffins"))
+            .bind(("license_number", license_number))
+            .await
+            .unwrap();
+
+        // create another license number
+        let license_number: usize = 678910;
+
+        // create another ew license
+        let sql = "CREATE registry:uuid() CONTENT { registration: $license_number }";
+
+        let _res = db
+            .query(sql)
+            .bind(("license_number", &license_number))
+            .await
+            .unwrap();
+
+        // relate another license to same person
+        let sql = "
+            LET $foo = SELECT id FROM person WHERE name = $name;
+            LET $bar = SELECT id FROM registry WHERE registration = $license_number;
+            RELATE $bar->licenses->$foo SET id = licenses:uuid();
+        ";
+
+        let _res = db
+            .query(sql)
+            .bind(("name", "McStuffins"))
+            .bind(("license_number", &license_number))
+            .await
+            .unwrap();
+
+        // Select id from person given a license number
+        let sql = "
+            LET $blah = SELECT id FROM registry WHERE registration = $license_number;
+            SELECT id, $blah->licenses->person from person;
+        ";
+
+        let mut res = db
+            .query(sql)
+            .bind(("license_number", license_number))
+            .await
+            .unwrap();
+        dbg!(&res);
+        let _ = res.take::<Option<Thing>>(0).unwrap();
+        let person_id = res.take::<Vec<Thing>>(1).unwrap();
+        dbg!(person_id);
     }
 }
